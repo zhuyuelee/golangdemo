fe30c62061ce28998079ab71c4a3c051b3aadc05
zhuyuelee <zhuyue@bitcms.net>
2020-10-09 17:24:05 +0800

    hello go

--- 1demo/main.go
+++ 1demo/main.go
@@ -2,29 +2,9 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
 )
 
 func main() {
 	//目标字符串
-	searchIn := "John: 2578.34458 William: 4567.23548 Steve: 5632.18127 "
-	pat := "[0-9]+.[0-9]+" //正则
-
-	f := func(s string) string {
-		v, _ := strconv.ParseFloat(s, 32)
-		return strconv.FormatFloat(v, 'f', 4, 32)
-	}
-
-	if ok, _ := regexp.Match(pat, []byte(searchIn)); ok {
-		fmt.Println("Match Found!")
-	}
-
-	re, _ := regexp.Compile(pat)
-	//将匹配到的部分替换为"##.#"
-	str := re.ReplaceAllString(searchIn, "##.#")
-	fmt.Println(str)
-	//参数为函数时
-	str2 := re.ReplaceAllStringFunc(searchIn, f)
-	fmt.Println(str2)
+	fmt.Println("hellw go")
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # golangdemo
+
+golang studay demo
